075778b88db9d9240f30799984d120f7e2264ef0
mrgoodrich <mryangoodrich@icloud.com>
2014-12-08 00:09:25 -0800

    Current version has a moving arrow, two penguins, and is now 1200x600

--- js/warfare.js
+++ js/warfare.js
@@ -1,11 +1,9 @@
 var canvas;
 var pen;
-var image;
 var mousepos = [0,500];
 var init = function(canvasID){
 	canvas = document.getElementById(canvasID);
 	pen = canvas.getContext("2d");
-	image = document.getElementById("scream");
 }
 
 var drawPenguins = function(){
@@ -17,36 +15,36 @@ var drawPenguins = function(){
 	pen1.onload = function(){
     	pen.save();
     	pen.scale(-1,1);
-    	pen.drawImage(pen2, 550, 600,50,50);
+    	pen.drawImage(pen2,-1150,550,50,50);
     	pen.restore();
 
   	}
   	pen2.onload = function(){
-		pen.drawImage(pen1, 0, 600,50,50);
+		pen.drawImage(pen1, 0, 550,50,50);
 
   	}
 }
 
-function drawRotatedImage(image, x, y, angle){ 
+function drawRotatedImage(image, x, y, angle, length){ 
     pen.save(); 
-    pen.translate(x, y); 
+    pen.translate(x-25, y-25); 
     pen.rotate(angle);
-    pen.drawImage(image, 0, 0,70,30);
+    pen.drawImage(image, 0, 0,10+length/5,30);
     pen.restore(); 
     //pen.drawImage(image,250,250,50,50);
 }
 
 
 var drawArrow = function(mousepos){
+  var distance = Math.sqrt((550-mousepos[1])*(550-mousepos[1])+(0-mousepos[0])*(0-mousepos[0]));
   base_image = new Image();
   base_image.src = 'arrow.png';
   base_image.onload = function(){
-    drawRotatedImage(base_image, 0, 550, Math.atan((mousepos[1]-500)/mousepos[0]));
+    drawRotatedImage(base_image, 0, 550, Math.atan((mousepos[1]-500)/mousepos[0]),distance);
   }
 }
 
 function writeMessage(message) {
-	pen.clearRect(0, 0, canvas.width, canvas.height);
 	pen.font = '18pt Calibri';
 	pen.fillStyle = 'black';
 	pen.fillText(message, 10, 25);
