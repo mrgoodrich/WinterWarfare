cbed7f56202023c30c6e9e9e4eb6831db4dfbd20
mrgoodrich <mryangoodrich@icloud.com>
2014-12-22 00:53:01 -0700

    Added a scoring system, started the main menu, added something to ensure one click doesnt move multiple steps, WORKING ON COLLISION DETECTION STILL

--- js/ball.js
+++ js/ball.js
@@ -4,27 +4,36 @@ var Ball = function(wind, currentpos, radius, otherplayer, velocity){ // we know
 	this.radius = radius; 
 	this.currentpos = currentpos;
 	this.velocity = velocity; // vector
+
 	this.step = function(){
 		this.velocity[1] -= .05; // gravity;
 		this.currentpos[0] += this.velocity[0];
 		this.currentpos[1] -= this.velocity[1];
 	}
 	this.touching = function(){
-		
-		if(this.currentpos[1]>600-this.radius){
-			return true;
+		if (this.currentpos[1]>600-this.radius){
+			return 1;
 		}
-		if(this.currentpos[0]>this.otherplayer[0]&&this.currentpos[0]<this.otherplayer[0]+50){
-			if(this.currentpos[1]>this.otherplayer[1]-50){
-				console.log('hit');
-				return true;
+		if (velocity > 1){  //See which way ball is moving for collision detection with respect to radius
+			if (this.currentpos[0]+this.radius>this.otherplayer[0]&&this.currentpos[0]+this.radius/2<this.otherplayer[0]+50){  //redo with draw respect to center of obj
+				if(this.currentpos[1]+this.radius>this.otherplayer[1]-50){
+					return 2;
+				}
 			}
 		}
-		return false;
+		else{
+			if (this.currentpos[0]<this.otherplayer[0]+50&&this.currentpos[0]>this.otherplayer[0]){
+				if(this.currentpos[1]+this.radius>this.otherplayer[1]-50){
+					return 2;
+				}
+			}
+		}
+		return 0;
 	}
 	this.draw = function(p){
 		p.beginPath();
 		p.arc(currentpos[0],currentpos[1],radius,0,2*Math.PI);
+		p.arc(0,0,radius,0,2*Math.PI);
 		p.stroke();
 	}
 
--- js/warfare.js
+++ js/warfare.js
@@ -12,8 +12,10 @@ var penboxsize = 50;
 var penguimg1 = new Image();
 var penguimg2 = new Image();
 var arroimg = new Image();
-var state = 0;
+var state = -1;
 var mouseDown = 0;
+var score = [0,0];
+var mDown = false; //boolean for main menu, important to ensure that the first shot isnt sent upon mouse click
 
 var init = function(canvasID){
 	canvas1 = document.getElementById("canvas");
@@ -45,13 +47,39 @@ var drawArrow = function(arrowpos, mousepos){
   pen.translate(arrowpos[0], arrowpos[1]); 
   pen.rotate(angle);
   pen.drawImage(arroimg, 0, -15,10+length/5,30);
-  pen.restore(); 
+  pen.restore();
+}
+
+function newPosition(peng){
+  if (peng == 1){
+    penguin1[0] = 10+580*Math.random();
+    arrowpos1[0] = penguin1[0] + 30;
+    arrowpoint1 = [arrowpos1[0],arrowpos1[1]-15];
+  }
+  else if (peng == 2){
+    penguin2[0] = 610+580*Math.random();
+    arrowpos2[0] = penguin2[0] - 30;
+    arrowpoint2 = [arrowpos2[0],arrowpos2[1]-15];
+  }
 }
 
 function writeMessage(message) {
 	pen.font = '18pt Calibri';
 	pen.fillStyle = 'black';
 	pen.fillText(message, 10, 25);
+}
+
+function writeScore(){
+  pen.font = '30pt Calibri';
+  pen.fillStyle = 'black';
+  pen.fillText(score[0], 300, 50);
+  pen.fillText(score[1], 900, 50);
+}
+
+function writeMainMenu(){
+  pen.font = '45pt Calibri';
+  pen.fillStyle = 'black';
+  pen.fillText("Winter Warfare", 500, 300);
 }
 
 function getMousePos(canvas, evt) {
@@ -64,9 +92,23 @@ function getMousePos(canvas, evt) {
 var b;
 var run = function(){
   draw();
-  if (state == 0){
+  if (state == -1){ //Main menu
+    writeMainMenu();
+    if (mouseDown > 0){
+      mDown = true;
+    }
+    if (mouseDown == 0 && mDown){
+      mDown = false;
+      state = 0;
+    }
+  }
+  else if (state == 0){
     drawArrow(arrowpos1,mousepos);
     if (mouseDown > 0){
+      mDown = true;
+    }
+    if (mouseDown == 0 && mDown){
+      mDown = false;
       state = 1
       velocity = [(mousepos[0]-arrowpoint1[0])/100, (arrowpoint1[1]-mousepos[1])/100];
       b = new Ball(0,[arrowpoint1[0],arrowpoint1[1]],15,penguin2,velocity);
@@ -74,12 +116,21 @@ var run = function(){
   }else if (state == 1){
     b.step();
     b.draw(pen);
-    if (b.touching()){
+    if (b.touching()==2){ //hit
+      state = 2;
+      score[0]++;
+      newPosition(2);
+    }
+    else if (b.touching()==1){ //miss
       state = 2;
     }
   }else if (state == 2){
     drawArrow(arrowpos2,mousepos);
     if (mouseDown > 0){
+      mDown = true;
+    }
+    if (mouseDown == 0 && mDown){
+      mDown = false;
       state = 3;
       velocity = [(mousepos[0]-arrowpoint2[0])/100, (arrowpoint2[1]-mousepos[1])/100];
       b = new Ball(0,[arrowpoint2[0],arrowpoint2[1]],15,penguin1,velocity);
@@ -87,7 +138,12 @@ var run = function(){
   }else if (state == 3){
     b.step();
     b.draw(pen);
-    if (b.touching()){
+    if (b.touching()==2){ //hit
+      state = 0;
+      score[1]++;
+      newPosition(1);
+    }
+    else if (b.touching()==1){ //miss
       state = 0;
     }
   }else{
@@ -100,6 +156,7 @@ var draw = function(){
   pen= buffers[drawingbuffer].getContext('2d'); // 0
   pen.clearRect(0, 0, canvas1.width, canvas1.height);
   writeMessage(mousepos);
+  writeScore();
 }
 var swap = function(){
   pen.drawImage(buffers[drawingbuffer],0,0);
